6b9f50906320a06452edb320f98b3d4b0a2918fc
FlyTigerDuan <flytigerduan@163.com>
2019-03-05 22:20:42 +0800

    提交文件

---
+++ src/main/java/com/dfh/enter/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.dfh.enter.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index(HttpServletRequest req,Model model) {
+		System.out.println("你好啊");
+		return "hello";
+	}
+
+	
+
+}
